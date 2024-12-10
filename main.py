1ac1d7b5ceb6f5ef39736231836a7e80c4d96546
Kaan Yılmaz <71845829+kaan6634@users.noreply.github.com>
2024-12-10 15:13:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+import face_recognition
+import cv2
+import numpy as np
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Load sample pictures and learn how to recognize them
+person1_image = face_recognition.load_image_file("person1.png")
+person1_face_encoding = face_recognition.face_encodings(person1_image)[0]
+
+person2_image = face_recognition.load_image_file("person2.png")
+person2_face_encoding = face_recognition.face_encodings(person2_image)[0]
+
+person3_image = face_recognition.load_image_file("person3.png")
+person3_face_encoding = face_recognition.face_encodings(person3_image)[0]
+
+person4_image = face_recognition.load_image_file("person4.png")
+person4_face_encoding = face_recognition.face_encodings(person4_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    person1_face_encoding,
+    person2_face_encoding,
+    person3_face_encoding,
+    person4_face_encoding
+]
+known_face_names = [
+    "person1",
+    "person2",
+    "person3",
+    "person4"
+]
+
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Flip the frame to create a mirror effect
+    frame = cv2.flip(frame, 1)
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # Use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
